630541f7304c2f015ff76a440f873c4b40ba4df5
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-02-15 11:55:32 +0000

    update gallery pictures data

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,35 +1,4 @@
-import imgNr1webp from '../assets/images/gallery/andriyko-podilnyk-unsplash.webp';
-import imgNr1png from '../assets/images/gallery/andriyko-podilnyk-unsplash.png';
-import imgNr2webp from '../assets/images/gallery/chan-swan-unsplash.webp';
-import imgNr2png from '../assets/images/gallery/chan-swan-unsplash.png';
-import imgNr3webp from '../assets/images/gallery/daniel-mccarthy-themccarthy-unsplash.webp';
-import imgNr3png from '../assets/images/gallery/daniel-mccarthy-themccarthy-unsplash.png';
-import imgNr4webp from '../assets/images/gallery/gabriel-miklos-unsplash.webp';
-import imgNr4png from '../assets/images/gallery/gabriel-miklos-unsplash.png';
-import imgNr5webp from '../assets/images/gallery/mariya-pexels.webp';
-import imgNr5png from '../assets/images/gallery/mariya-pexels.png';
-import imgNr6webp from '../assets/images/gallery/janan-lagerwall-unsplash.webp';
-import imgNr6png from '../assets/images/gallery/janan-lagerwall-unsplash.png';
-import imgNr7webp from '../assets/images/gallery/li-yan-unsplash.webp';
-import imgNr7png from '../assets/images/gallery/li-yan-unsplash.png';
-import imgNr8webp from '../assets/images/gallery/matt-pike-unsplash.webp';
-import imgNr8png from '../assets/images/gallery/matt-pike-unsplash.png';
-import imgNr9webp from '../assets/images/gallery/misha-walker-unsplash.webp';
-import imgNr9png from '../assets/images/gallery/misha-walker-unsplash.png';
-import imgNr10webp from '../assets/images/gallery/pablo-martinez-unsplash.webp';
-import imgNr10png from '../assets/images/gallery/pablo-martinez-unsplash.png';
-import imgNr11webp from '../assets/images/gallery/sandy-millar-unsplash.webp';
-import imgNr11png from '../assets/images/gallery/sandy-millar-unsplash.png';
-import imgNr12webp from '../assets/images/gallery/seyma-dalar-pexels.webp';
-import imgNr12png from '../assets/images/gallery/seyma-dalar-pexels.png';
-import imgNr13webp from '../assets/images/gallery/pure-julia-unsplash.webp';
-import imgNr13png from '../assets/images/gallery/pure-julia-unsplash.png';
-import imgNr14webp from '../assets/images/gallery/jackob-amson-unsplash.webp';
-import imgNr14png from '../assets/images/gallery/jackob-amson-unsplash.png';
-import imgNr15webp from '../assets/images/gallery/tima-miroshnichenko-pexels.webp';
-import imgNr15png from '../assets/images/gallery/tima-miroshnichenko-pexels.png';
-import imgNr16webp from '../assets/images/gallery/satyabratasm-unsplash.webp';
-import imgNr16png from '../assets/images/gallery/satyabratasm-unsplash.png';
+import { pictures } from '../assets/data/pictures-data';
 
 import classes from './Gallery.module.css';
 
@@ -64,230 +33,24 @@ const Gallery = () => {
 
     return (
 			<div className={classes.gallery}>
-				<Picture
-					key={1}
+				{ pictures.map((picture, idx) => {
+					return (
+					<Picture
+					key={idx}
 					sources={[
-						{ type: webp, srcSet: imgNr1webp },
-						{ type: png, srcSet: imgNr1png },
+						{ type: webp, srcSet: picture.srcSetwebp },
+						{ type: png, srcSet: picture.srcSetpng },
 					]}
 					img={{
-						src: imgNr1webp,
+						src: picture.srcSetwebp,
 						width: width,
-						height: '363',
-						alt: 'cute bunnies nr1',
+						height: picture.height,
+						alt: `cute bunnies nr${idx}`
 					}}
-					dataName="Miffy & Olive"
-				/>
-				<Picture
-					key={2}
-					sources={[
-						{ type: webp, srcSet: imgNr2webp },
-						{ type: png, srcSet: imgNr2png },
-					]}
-					img={{
-						src: imgNr2webp,
-						width: width,
-						height: '392',
-						alt: 'cute bunny nr2',
-					}}
-					dataName="Thumper"
-				/>
-				<Picture
-					key={3}
-					sources={[
-						{ type: webp, srcSet: imgNr3webp },
-						{ type: png, srcSet: imgNr3png },
-					]}
-					img={{
-						src: imgNr3webp,
-						width: width,
-						height: '403',
-						alt: 'cute bunny nr3',
-					}}
-					dataName="Cinnabun"
-				/>
-				<Picture
-					key={4}
-					sources={[
-						{ type: webp, srcSet: imgNr4webp },
-						{ type: png, srcSet: imgNr4png },
-					]}
-					img={{
-						src: imgNr4webp,
-						width: width,
-						height: '404',
-						alt: 'cute bunny nr4',
-					}}
-					dataName="Daisy"
-				/>
-				<Picture
-					key={5}
-					sources={[
-						{ type: webp, srcSet: imgNr5webp },
-						{ type: png, srcSet: imgNr5png },
-					]}
-					img={{
-						src: imgNr5webp,
-						width: width,
-						height: '242',
-						alt: 'cute bunnies nr5',
-					}}
-					dataName="Brno & Tom"
-				/>
-				<Picture
-					key={6}
-					sources={[
-						{ type: webp, srcSet: imgNr6webp },
-						{ type: png, srcSet: imgNr6png },
-					]}
-					img={{
-						src: imgNr6webp,
-						width: width,
-						height: '394',
-						alt: 'cute bunny nr6',
-					}}
-					dataName="Holly"
-				/>
-				<Picture
-					key={7}
-					sources={[
-						{ type: webp, srcSet: imgNr7webp },
-						{ type: png, srcSet: imgNr7png },
-					]}
-					img={{
-						src: imgNr7webp,
-						width: width,
-						height: '354',
-						alt: 'cute bunny nr7',
-					}}
-					dataName="Oreo"
-				/>
-				<Picture
-					key={8}
-					sources={[
-						{ type: webp, srcSet: imgNr8webp },
-						{ type: png, srcSet: imgNr8png },
-					]}
-					img={{
-						src: imgNr8webp,
-						width: width,
-						height: '392',
-						alt: 'cute bunny nr8',
-					}}
-					dataName="Lola"
-				/>
-				<Picture
-					key={9}
-					sources={[
-						{ type: webp, srcSet: imgNr9webp },
-						{ type: png, srcSet: imgNr9png },
-					]}
-					img={{
-						src: imgNr9webp,
-						width: width,
-						height: '354',
-						alt: 'cute bunny nr9',
-					}}
-					dataName="Chester"
-				/>
-				<Picture
-					key={10}
-					sources={[
-						{ type: webp, srcSet: imgNr10webp },
-						{ type: png, srcSet: imgNr10png },
-					]}
-					img={{
-						src: imgNr10webp,
-						width: width,
-						height: '427',
-						alt: 'cute bunny nr10',
-					}}
-					dataName="Snowball"
-				/>
-				<Picture
-					key={11}
-					sources={[
-						{ type: webp, srcSet: imgNr11webp },
-						{ type: png, srcSet: imgNr11png },
-					]}
-					img={{
-						src: imgNr11webp,
-						width: width,
-						height: '394',
-						alt: 'cute bunny nr11',
-					}}
-					dataName="Hopper"
-				/>
-				<Picture
-					key={12}
-					sources={[
-						{ type: webp, srcSet: imgNr12webp },
-						{ type: png, srcSet: imgNr12png },
-					]}
-					img={{
-						src: imgNr12webp,
-						width: width,
-						height: '473',
-						alt: 'cute bunnies nr12',
-					}}
-					dataName="Jack & Jill"
-				/>
-				<Picture
-					key={13}
-					sources={[
-						{ type: webp, srcSet: imgNr13webp },
-						{ type: png, srcSet: imgNr13png },
-					]}
-					img={{
-						src: imgNr13webp,
-						width: width,
-						height: '473',
-						alt: 'cute bunnies nr13',
-					}}
-					dataName="Fish & Chips"
-				/>
-				<Picture
-					key={14}
-					sources={[
-						{ type: webp, srcSet: imgNr14webp },
-						{ type: png, srcSet: imgNr14png },
-					]}
-					img={{
-						src: imgNr14webp,
-						width: width,
-						height: '354',
-						alt: 'cute bunny nr14',
-					}}
-					dataName="Oliver"
-				/>
-				<Picture
-					key={15}
-					sources={[
-						{ type: webp, srcSet: imgNr15webp },
-						{ type: png, srcSet: imgNr15png },
-					]}
-					img={{
-						src: imgNr15webp,
-						width: width,
-						height: '285',
-						alt: 'cute bunnies nr15',
-					}}
-					dataName="Fluff & Bunbun"
-				/>
-				<Picture
-					key={16}
-					sources={[
-						{ type: webp, srcSet: imgNr16webp },
-						{ type: png, srcSet: imgNr16png },
-					]}
-					img={{
-						src: imgNr16webp,
-						width: width,
-						height: '392',
-						alt: 'cute bunny nr16',
-					}}
-					dataName="Teddy"
-				/>
+					dataName={picture.dataName}
+					/>)
+				}
+				)}
 			</div>
 		);
                    
---
+++ src/assets/data/pictures-data.js
@@ -0,0 +1,131 @@
+import imgNr1webp from '../images/gallery/andriyko-podilnyk-unsplash.webp';
+import imgNr1png from '../images/gallery/andriyko-podilnyk-unsplash.png';
+import imgNr2webp from '../images/gallery/chan-swan-unsplash.webp';
+import imgNr2png from '../images/gallery/chan-swan-unsplash.png';
+import imgNr3webp from '../images/gallery/daniel-mccarthy-themccarthy-unsplash.webp';
+import imgNr3png from '../images/gallery/daniel-mccarthy-themccarthy-unsplash.png';
+import imgNr4webp from '../images/gallery/gabriel-miklos-unsplash.webp';
+import imgNr4png from '../images/gallery/gabriel-miklos-unsplash.png';
+import imgNr5webp from '../images/gallery/mariya-pexels.webp';
+import imgNr5png from '../images/gallery/mariya-pexels.png';
+import imgNr6webp from '../images/gallery/janan-lagerwall-unsplash.webp';
+import imgNr6png from '../images/gallery/janan-lagerwall-unsplash.png';
+import imgNr7webp from '../images/gallery/li-yan-unsplash.webp';
+import imgNr7png from '../images/gallery/li-yan-unsplash.png';
+import imgNr8webp from '../images/gallery/matt-pike-unsplash.webp';
+import imgNr8png from '../images/gallery/matt-pike-unsplash.png';
+import imgNr9webp from '../images/gallery/misha-walker-unsplash.webp';
+import imgNr9png from '../images/gallery/misha-walker-unsplash.png';
+import imgNr10webp from '../images/gallery/pablo-martinez-unsplash.webp';
+import imgNr10png from '../images/gallery/pablo-martinez-unsplash.png';
+import imgNr11webp from '../images/gallery/sandy-millar-unsplash.webp';
+import imgNr11png from '../images/gallery/sandy-millar-unsplash.png';
+import imgNr12webp from '../images/gallery/seyma-dalar-pexels.webp';
+import imgNr12png from '../images/gallery/seyma-dalar-pexels.png';
+import imgNr13webp from '../images/gallery/pure-julia-unsplash.webp';
+import imgNr13png from '../images/gallery/pure-julia-unsplash.png';
+import imgNr14webp from '../images/gallery/jackob-amson-unsplash.webp';
+import imgNr14png from '../images/gallery/jackob-amson-unsplash.png';
+import imgNr15webp from '../images/gallery/tima-miroshnichenko-pexels.webp';
+import imgNr15png from '../images/gallery/tima-miroshnichenko-pexels.png';
+import imgNr16webp from '../images/gallery/satyabratasm-unsplash.webp';
+import imgNr16png from '../images/gallery/satyabratasm-unsplash.png';
+
+export const pictures = [
+	{
+		srcSetwebp: imgNr1webp,
+		srcSetpng: imgNr1png,
+		height: '363',
+		dataName: 'Miffy & Olive',
+	},
+	{
+		srcSetwebp: imgNr2webp,
+		srcSetpng: imgNr2png,
+		height: '392',
+		dataName: 'Thumper',
+	},
+	{
+		srcSetwebp: imgNr3webp,
+		srcSetpng: imgNr3png,
+		height: '403',
+		dataName: 'Cinnabun',
+	},
+	{
+		srcSetwebp: imgNr4webp,
+		srcSetpng: imgNr4png,
+		height: '404',
+		dataName: 'Daisy',
+	},
+	{
+		srcSetwebp: imgNr5webp,
+		srcSetpng: imgNr5png,
+		height: '242',
+		dataName: 'Brno & Tom',
+	},
+	{
+		srcSetwebp: imgNr6webp,
+		srcSetpng: imgNr6png,
+		height: '394',
+		dataName: 'Holly',
+	},
+	{
+		srcSetwebp: imgNr7webp,
+		srcSetpng: imgNr7png,
+		height: '354',
+		dataName: 'Oreo',
+	},
+	{
+		srcSetwebp: imgNr8webp,
+		srcSetpng: imgNr8png,
+		height: '392',
+		dataName: 'Lola',
+	},
+	{
+		srcSetwebp: imgNr9webp,
+		srcSetpng: imgNr9png,
+		height: '354',
+		dataName: 'Chester',
+	},
+	{
+		srcSetwebp: imgNr10webp,
+		srcSetpng: imgNr10png,
+		height: '427',
+		dataName: 'Snowball',
+	},
+	{
+		srcSetwebp: imgNr11webp,
+		srcSetpng: imgNr11png,
+		height: '394',
+		dataName: 'Hopper',
+	},
+	{
+		srcSetwebp: imgNr12webp,
+		srcSetpng: imgNr12png,
+		height: '473',
+		dataName: 'Jack & Jill',
+	},
+	{
+		srcSetwebp: imgNr13webp,
+		srcSetpng: imgNr13png,
+		height: '473',
+		dataName: 'Fish & Chips',
+	},
+	{
+		srcSetwebp: imgNr14webp,
+		srcSetpng: imgNr14png,
+		height: '354',
+		dataName: 'Oliver',
+	},
+	{
+		srcSetwebp: imgNr15webp,
+		srcSetpng: imgNr15png,
+		height: '285',
+		dataName: 'Fluff & Bunbun',
+	},
+	{
+		srcSetwebp: imgNr16webp,
+		srcSetpng: imgNr16png,
+		height: '392',
+		dataName: 'Teddy',
+	},
+];
