792a5f02f52fddd4d527d3f9aa30e9a784555e8c
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-03-17 11:47:51 +0000

    add settings for rest-framework, database, cloudinary and whitenoise

--- backend/rabbit_rescue_REST/settings.py
+++ backend/rabbit_rescue_REST/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("BACKEND_HOST"), "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -40,16 +41,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',  # for cloudinary
+    'cloudinary',  # for cloudinary
+    'rest_framework',  # for djangorestframework
+    'corsheaders',  # new
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # new
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # new
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
 ]
 
 ROOT_URLCONF = 'rabbit_rescue_REST.urls'
@@ -57,7 +66,7 @@ ROOT_URLCONF = 'rabbit_rescue_REST.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,30 +85,37 @@ WSGI_APPLICATION = 'rabbit_rescue_REST.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'SQLITE3' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL', ''))
+    }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+ {
+  'NAME':
+  'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+ },
+ {
+  'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+ },
+ {
+  'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+ },
+ {
+  'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+ },
 ]
 
 
@@ -122,7 +138,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# for WhiteNoise to serve static files in production with 'collectstatic'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )  # has to be a tuple
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cloudinary settings
+# https://cloudinary.com/documentation/django_image_and_video_upload#configuration
+
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.getenv("CLOUDINARY_URL"),
+}
+MEDIA_URL = '/rabbits/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
