abbec3963330ce4c981e27a1826e46eaeb206702
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-04-02 19:40:58 +0100

    update Footer test text

--- frontend/src/Components/Footer.test.jsx
+++ frontend/src/Components/Footer.test.jsx
@@ -2,7 +2,7 @@ import { test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Footer from './Footer'
 
-test('renders follow text', () => {
+test('renders footer phone link', () => {
   render(<Footer />)
   const linkElement = screen.getByRole("link", { name: /phone icon/i })
   expect(linkElement).toBeVisible()
