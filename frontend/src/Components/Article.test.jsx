b3e150dd9ca6420a532e06cc89d23639ff4d9087
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-04-02 19:54:53 +0100

    edit test for Article

--- frontend/src/Components/Article.test.jsx
+++ frontend/src/Components/Article.test.jsx
@@ -1,20 +1,26 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import { ArticleLeft, ArticleRight } from './Article'
+import { Article, ArticleLeft, ArticleRight } from './Article'
 
 test('renders follow text', () => {
   render(
     <>
+      <Article>Follow us on social media</Article>
       <ArticleLeft>Left content</ArticleLeft>
       <ArticleRight>Right content</ArticleRight>
     </>
   )
+  const article = screen.getByText(/Follow us on social media/i)
   const articleLeft = screen.getByText(/Left content/i)
   const articleRight = screen.getByText(/Right content/i)
+
+  expect(article).toBeVisible()
   expect(articleLeft).toBeVisible()
   expect(articleRight).toBeVisible()
+
   expect(articleLeft.className).toMatch(/article/i)
   expect(articleRight.className).toMatch(/article/i)
-  expect(articleLeft).toHaveStyle({ 'display': 'flex' })
+  
+  expect(article).toHaveStyle({ 'display': 'flex' })
 })
 
