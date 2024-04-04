ccbcdc997f99593b2c73f224c93f89ddb7231f1f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-04-04 13:59:56 +0100

    test Burger icon click flow

--- frontend/src/Components/Burger.test.jsx
+++ frontend/src/Components/Burger.test.jsx
@@ -1,17 +1,46 @@
 import { test } from 'vitest'
-import { render, screen, getRoles } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { Burger } from './Burger'
 import { Provider } from 'react-redux'
 import store from '../store'
 
 test('renders Burger', () => {
-  const { container } = render(
+  render(
     <Provider store={store}>
         <Burger />
     </Provider>
   )
-  console.log(getRoles(container))
+
+  // find the burger button
   const burgerButton = screen.getByRole("button", { name: /burger/i })
+
+  // check if the button is visible
   expect(burgerButton).toBeVisible()
+
+})
+
+test('Burger icon click flow', () => {
+
+  render(
+    <Provider store={store}>
+        <Burger />
+    </Provider>
+  )
+
+  // find the burger button
+  const burgerButton = screen.getByRole("button", { name: /burger/i })
+
+  // click the button
+  fireEvent.click(burgerButton);
+
+  // check if burger state is true
+  expect(store.getState().burger).toBe(true);
+
+  // click the button again
+  fireEvent.click(burgerButton);
+
+  // check if burger state is false
+  expect(store.getState().burger).toBe(false);
+
 })
 
