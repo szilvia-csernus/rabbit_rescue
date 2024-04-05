7f763d0cfce179c01537972777cad4fae345807d
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-04-05 10:37:26 +0100

    remove code for debugging

--- frontend/src/Components/NavBar.test.jsx
+++ frontend/src/Components/NavBar.test.jsx
@@ -1,19 +1,19 @@
 import { test } from 'vitest'
-import { render, screen, getRoles } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import NavBar from './NavBar'
 import { Provider } from 'react-redux'
 import store from '../store'
 
-test('renders rabbit logo', () => {
-  const { container } = render(
+test('renders NavBar', () => {
+  render(
     <Provider store={store}>
       <MemoryRouter>
         <NavBar />
       </MemoryRouter>
     </Provider>
   )
-  console.log(getRoles(container))
+  
   const aboutLink = screen.getByRole("link", { name: /about/i })
   const adoptLink = screen.getByRole("link", { name: /adopt/i })
   const contactLink = screen.getByRole("link", { name: /contact/i })
@@ -23,4 +23,3 @@ test('renders rabbit logo', () => {
   expect(contactLink).toBeVisible()
   expect(donateButton).toBeVisible()
 })
-
