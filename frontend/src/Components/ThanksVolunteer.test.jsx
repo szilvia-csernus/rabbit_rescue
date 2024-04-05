539f572294e1f6acc55317eb1dae5df88d7a8113
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-04-05 11:02:52 +0100

    add test to ThanksVolunteer component

---
+++ frontend/src/Components/ThanksVolunteer.test.jsx
@@ -0,0 +1,36 @@
+import { test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import ThanksVolunteer from './ThanksVolunteer'
+import { Provider } from 'react-redux'
+import store from '../store'
+
+let modalRoot;
+
+beforeEach(() => {
+  // Create a div that we can attach the modal to.
+  modalRoot = document.createElement('div');
+  modalRoot.setAttribute('id', 'overlay');
+  document.body.appendChild(modalRoot);
+});
+
+afterEach(() => {
+  // Clean up on exiting
+  document.body.removeChild(modalRoot);
+  modalRoot = null;
+});
+
+test('renders ThanksVolunteer modal', () => {
+  render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <ThanksVolunteer />
+      </MemoryRouter>
+    </Provider>
+  )
+  
+  const heading = screen.getByRole("heading", { name: /thanks for your interest/i })
+  
+  expect(heading).toBeVisible()
+  
+})
