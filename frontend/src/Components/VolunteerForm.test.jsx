8219c7e02511e346f1778b71833165cc5d5fa8cf
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-04-05 17:06:47 +0100

    add tests for VolunteerForm component

---
+++ frontend/src/Components/VolunteerForm.test.jsx
@@ -0,0 +1,84 @@
+import { test } from 'vitest'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import VolunteerForm from './VolunteerForm'
+import { Provider } from 'react-redux'
+import store from '../store'
+
+
+let modalRoot;
+
+beforeEach(() => {
+  // Create a div that we can attach the modal to.
+  modalRoot = document.createElement('div');
+  modalRoot.setAttribute('id', 'overlay');
+  document.body.appendChild(modalRoot);
+});
+
+afterEach(() => {
+  // Clean up on exiting
+  document.body.removeChild(modalRoot);
+  modalRoot = null;
+});
+
+test('renders VolunteerForm and its elements into modal', () => {
+  const { container } = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <VolunteerForm />
+      </MemoryRouter>
+    </Provider>
+  )
+  
+  // Check that the title is visible
+  const heading = screen.getByRole("heading", { name: /volunteer/i })
+  expect(heading).toBeVisible()
+
+  // Check that the name input is visible
+  const nameInput = screen.getByRole("textbox", { name: /name/i })
+  expect(nameInput).toBeVisible()
+
+  // Check that the email input is visible
+  const emailInput = screen.getByRole("textbox", { name: /email/i })
+  expect(emailInput).toBeVisible()
+
+  // Check that the phone input is visible
+  const phoneInput = screen.getByRole("textbox", { name: /phone/i })
+  expect(phoneInput).toBeVisible()
+
+  // Check that the message input is visible
+  const messageInput = screen.getByRole("textbox", { name: /please tell us about/i })
+  expect(messageInput).toBeVisible()
+
+  // Check that the cancel button is visible
+  const cancel = screen.getByRole("button", { name: /cancel/i })
+  expect(cancel).toBeVisible()
+
+  // Check that the send button is visible
+  const send = screen.getByRole("button", { name: /send/i })
+  expect(send).toBeVisible()
+})
+
+test('Volunteer form submission with insufficient data renders error messages', () => {
+  const { container } = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <VolunteerForm />
+      </MemoryRouter>
+    </Provider>
+  )
+
+  // Get the send button
+  const send = screen.getByRole("button", { name: /send/i })
+
+  // Click the send button
+  fireEvent.click(send);
+
+  // Check that the name error message is visible
+  const nameError = screen.getByText(/please provide your name/i)
+  expect(nameError).toBeVisible()
+
+  // Check that the email error message is visible
+  const emailError = screen.getByText(/please provide your email/i)
+  expect(emailError).toBeVisible()
+})
