6d1b5478fafd4de698791799142b98596eef451c
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-04-19 19:17:03 +0100

    add cypress-vite package

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
+import vitePreprocessor from 'cypress-vite'
 
 export default defineConfig({
   projectId: 'rd7g75',
   e2e: {
     baseUrl: "http://localhost:5173",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      on('file:preprocessor', vitePreprocessor())
     },
   },
 
